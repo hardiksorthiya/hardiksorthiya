9e636bc057c41b29470fdbc6401df492409e7108
Hardik Sorathiya <91784863+hardiksorthiya@users.noreply.github.com>
2022-03-23 10:03:38 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,274 @@
+
+    
+    function lighting() {
+             console.log('Smart Lighting Drop Down activated')
+        }
+    document.getElementById("environment").addEventListener("click", environment);
+       function environment() {
+        console.log('Environment Drop Down activated')
+        }
+        document.getElementById("visualeffects").addEventListener("click", visualeffects);
+    function visualeffects() {
+        console.log('Visual Effect Drop Down activated')
+        }
+        
+   function smartlighting() {
+        console.log('Smart Lighting Drop Down activated')
+        }
+
+    // =================================================================================================
+
+    document.getElementById("test").addEventListener("click", View1);
+    function View1() {
+                   console.log('Lighting First view activated')
+       }
+
+    document.getElementById("profile-tab").addEventListener("click", View2);
+        function View2() {
+                  console.log('Lighting Second view activated')
+       }
+    document.getElementById("contact-tab").addEventListener("click", View3);
+    function View3() {
+                 console.log('Lighting Third view activated')
+       }
+
+
+    //=========================================================
+
+
+    document.getElementById("viewwalllampli_1").addEventListener("click", viewwalllampli_1);
+    function viewwalllampli_1() {
+                 console.log('Adjust Intensity of lighting parameter for Wall Lamp Light(L) of view 1')
+       }
+
+    document.getElementById("viewwalllamplc_1").addEventListener("click", viewwalllamplc_1);
+    function viewwalllamplc_1() {
+                console.log('Choose Color from colour Picker for Wall Lamp Light(L) of view 1')
+       }
+    document.getElementById("button-18").addEventListener("click", buttonwalllampl);
+       function buttonwalllampl() {
+                 console.log('Switch ON/OFF button For Wall Lamp Light(L) of view 1')
+       }
+
+
+    //    ============================================
+
+
+    document.getElementById("viewwalllampli_2").addEventListener("click", viewwalllampli_2);
+    function viewwalllampli_2() {
+        
+           console.log('Adjust Intensity of lighting parameter for Wall Lamp Light(R) of view 1')
+       }
+    document.getElementById("viewwalllamplc_2").addEventListener("click", viewwalllamplc_2);
+    function viewwalllamplc_2() {
+                  console.log('Choose Color from colour Picker for Wall Lamp Light(R) of view 1')
+       }
+    document.getElementById("button-18-1").addEventListener("click", buttonwalllamp2);  
+    function buttonwalllamp2() {
+           console.log('Switch ON/OFF button For Wall Lamp Light(R) of view 1')
+       }
+
+
+    //    ===========================================
+
+    document.getElementById("viewcellingi1").addEventListener("click", viewcellingi1);
+    function viewcellingi1() {
+        console.log('Adjust Intensity of lighting parameter for Celling Light of view 1')
+    }
+    document.getElementById("viewcellingc1").addEventListener("click", viewcellingc1);
+    function viewcellingc1() {
+        console.log('Choose Color from colour Picker for Celling Light of view 1')
+    }
+    document.getElementById("button-18-2").addEventListener("click", buttoncelling1);
+    function buttoncelling1() {
+        console.log('Switch ON/OFF button For Celling Light of view 1')
+    }
+
+
+    // ===========================================
+
+
+    document.getElementById("viewchandeli1").addEventListener("click", viewchandeli1);
+    function viewchandeli1() {
+        console.log('Adjust Intensity of lighting parameter for Decoration Light of view 1')
+    }
+    document.getElementById("viewchandelc1").addEventListener("click", viewchandelc1);
+    function viewchandelc1() {
+        console.log('Choose Color from colour Picker for Decoration Light of view 1')
+    }
+    document.getElementById("button-18-3").addEventListener("click", buttonchandel1);
+    function buttonchandel1() {
+        console.log('Switch ON/OFF button For Decoration Light of view 1')
+    }
+
+
+    // ===========================================
+
+
+    document.getElementById("viewdecori1").addEventListener("click", viewdecori1);
+    function viewdecori1() {
+        console.log('Adjust Intensity of lighting parameter for Decoration Light of view 1')
+    }
+    document.getElementById("viewdecorc1").addEventListener("click", viewdecorc1);
+    function viewdecorc1() {
+        console.log('Choose Color from colour Picker for Decoration Light of view 1')
+    }
+    document.getElementById("button-18-4").addEventListener("click", buttondecor1);
+    function buttondecor1() {
+        console.log('Switch ON/OFF button For Decoration Light of view 1')
+    }
+
+
+    // ==============================================
+
+
+    document.getElementById("viewcabineti1").addEventListener("click", viewcabineti1);
+    function viewcabineti1() {
+        console.log('Adjust Intensity of lighting parameter for Cabinet Light of view 1')
+    }
+    document.getElementById("viewcabinetc1").addEventListener("click", viewcabinetc1);
+    function viewcabinetc1() {
+        console.log('Choose Color from colour Picker for Cabinet Light of view 1')
+    }
+    document.getElementById("button-18-5").addEventListener("click", buttoncabinet1);
+    function buttoncabinet1() {
+        console.log('Switch ON/OFF button For Cabinet Light of view 1')
+    }
+
+    //============================================================  
+    document.getElementById("button-18-6").addEventListener("click", alllightbtnview1);
+    function alllightbtnview1() {
+        console.log('Switch ON/OFF button for All Lights of view 1')
+
+          }
+
+    // ============================================================
+    document.getElementById("viewcellingi2").addEventListener("click", viewcellingi2);
+    function viewcellingi2() {
+        console.log('Adjust Intensity of lighting parameter for Ceiling Light of view 2')
+    }
+    document.getElementById("viewcellingc2").addEventListener("click", viewcellingc2);
+    function viewcellingc2() {
+        console.log('Choose Color from colour Picker for Ceiling Light of view 2')
+    }
+    document.getElementById("button-18-7").addEventListener("click", buttoncelling2);
+    function buttoncelling2() {
+        console.log('Switch ON/OFF button for Ceiling Light of view 2')
+    }
+
+    // ==============================================================
+
+    document.getElementById("viewcabineti2").addEventListener("click", viewcabineti2);
+    function viewcabineti2() {
+        console.log('Adjust Intensity of lighting parameter for Cabinet Light of view 2')
+    }
+    document.getElementById("viewcabinetc2").addEventListener("click", viewcabinetc2);
+    function viewcabinetc2() {
+        console.log('Choose Color from colour Picker for Cabinet Light of view 2')
+    }
+    document.getElementById("button-18-8").addEventListener("click", buttoncabinet2);
+    function buttoncabinet2() {
+        console.log('Switch ON/OFF button For Cabinet Light of view 2')
+    }
+
+    // ======================================================================
+
+    document.getElementById("button-18-9").addEventListener("click", alllightbtnview2);
+    function alllightbtnview2() {
+        console.log('Switch ON/OFF button for All Lights of view 2')
+        }
+
+    // ===========================================================================
+    document.getElementById("viewwalllampli_3").addEventListener("click", viewwalllampli_3);
+    function viewwalllampli_3() {
+        console.log('Adjust Intensity of lighting parameter for Wall Lamp Light of view 3')
+    }
+    document.getElementById("viewwalllamplc_3").addEventListener("click", viewwalllamplc_3);
+    function viewwalllamplc_3() {
+        console.log('Choose Color from colour Picker for Wall Lamp Light of view 3')
+    }
+    document.getElementById("button-18-10").addEventListener("click", buttonwalllamp3);
+    function buttonwalllamp3() {
+        console.log('Switch ON/OFF button For Wall Lamp Light of view 3')
+    }
+
+    // ==================================================================================
+    
+    function viewdecori3() {
+        console.log('Adjust Intensity of lighting parameter for Decoration Light of view 3')
+    }
+    
+    function viewdecorc3() {
+        console.log('Choose Color from colour Picker for Decoration Light of view 3')
+    }
+    document.getElementById("button-18-11").addEventListener("click", buttonwalllamp3);
+    function buttonwalllamp3() {
+        console.log('Switch ON/OFF button For Decoration Light of view 3')
+    }
+
+    //============================================================================
+
+    document.getElementById("button-18-12").addEventListener("click", alllightbtnview3);
+    function alllightbtnview3() {
+        console.log('Switch ON/OFF button For ALL Lights of view 3')
+    }
+
+    // ==============================================================================
+
+    document.getElementById("environmenttab").addEventListener("click", environmenttab);
+    function environmenttab() {
+        console.log('Select the environment Parameter from dropdown tab')
+    }
+
+    // =============================================================================
+
+    document.getElementById("visualshadow").addEventListener("click", visualshadow);
+       function visualshadow() {
+        console.log('Visual Shadow checkbox activated')
+    }
+    document.getElementById("visualreflection").addEventListener("click", visualreflection);
+    function visualreflection() {
+        console.log('Visual Reflection checkbox activated')
+    }
+
+    // =================================================================
+    
+    document.getElementById("chandeliercheck").addEventListener("click", chandeliercheck);
+    function chandeliercheck() {
+        console.log('chandelier checkbox activated')
+    }
+
+    // ==================================================================
+
+    document.getElementById("bulblm").addEventListener("click", bulblm);
+    function bulblm() {
+        console.log('Select the Bulb Luminous from dropdown tab')
+    }
+
+    // =========================================================================
+
+    document.getElementById("smartenvironment").addEventListener("click", smartenvironment);
+    function smartenvironment() {
+        console.log('Select the Environment for Smart Light from dropdown tab')
+    }
+
+    // ===========================================================================================================
+
+
+    const button = document.getElementById('button-18')
+    if(imgi.display != 'none'){
+        imgi.display != 'none';
+    }
+    else{
+        imgi.display != 'block';
+    }
+
+
+    document.getElementById("button-18").addEventListener("click", lighton);
+    document.getElementById("button-18").addEventListener("click", lightoff);
+    function lighton(){
+        document.getElementById("imgi").style.backgroundColor = "green";
+    }
+    function lightoff(){
+        document.getElementById("imgi").removeEventListener("click", lighton);
+    }
